9dd261c82dcf97a2eddd538defab25963487c241
Jinhwan1151 <165783856+Jinhwan1151@users.noreply.github.com>
2024-04-02 20:51:26 +0900

    add & divide work done

--- calc.py
+++ calc.py
@@ -1,2 +1,15 @@
 def main():
+
+def add(x,y): 
+     return x+y;
+
+def divide(x,y):
+    if y==0:
+        print("Error: cannot divide by zero.");
+    else:
+        print("%d / %d = %0.3f", (x,y,divide(x,y)));
+
+
+
+     
     
